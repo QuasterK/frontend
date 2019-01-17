07f673d67cc7d230a7dc555241ab03d1bb68974e
Piotr Komorowski <komorowski.piotr.91@gmail.com>
2019-01-17 23:12:47 +0100

    Updates

---
+++ precache-manifest.d08086fa04a18e26f7c1ac859f2113c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cbbaf94823758d3c60e",
+    "url": "/frontend/static/js/runtime~main.5cbbaf94.js"
+  },
+  {
+    "revision": "148c92929d4e27382f39",
+    "url": "/frontend/static/js/main.148c9292.chunk.js"
+  },
+  {
+    "revision": "17a657305cef9b7eda90",
+    "url": "/frontend/static/js/1.17a65730.chunk.js"
+  },
+  {
+    "revision": "148c92929d4e27382f39",
+    "url": "/frontend/static/css/main.b2644d9f.chunk.css"
+  },
+  {
+    "revision": "2ca27ae9250839ba74c6c9bcef9e398a",
+    "url": "/frontend/index.html"
+  }
+];
